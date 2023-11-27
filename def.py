6706f5c79ccd533e3dbbb6a88117f30e31d1c6c6
Maheshwar Anup <137691824+djmahe4@users.noreply.github.com>
2023-11-27 15:48:02 +0530

    Update and rename test2.py to def.py

---
+++ def.py
@@ -0,0 +1,32 @@
+from pytube import YouTube
+import os
+from moviepy.editor import AudioFileClip
+import json
+from youtubesearchpython import SearchVideos
+
+def download_audio(url, output_path):
+    # Download the video
+    video = YouTube(url)
+    stream = video.streams.filter(only_audio=True).first()
+    output_file = stream.download(output_path=output_path)
+
+    # Convert the file to mp3
+    clip = AudioFileClip(output_file)
+    clip.write_audiofile(output_file.replace(".mp4", ".mp3"))
+
+    # Remove the original file
+    os.remove(output_file)
+
+def search_youtube(song):
+    search = SearchVideos(song, offset = 1, mode = "json", max_results = 1)
+    results = search.result()
+    results_dict = json.loads(results)
+    if results_dict['search_result']:
+        first_result = results_dict['search_result'][0]
+        url = first_result['link']
+        return url
+    else:
+        return "No results found."
+
+# Use the function
+#download_audio("https://www.youtube.com/watch?v=fJ9rUzIMcZQ", "/path/to/output/directory")

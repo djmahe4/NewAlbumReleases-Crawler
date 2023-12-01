cc9d9d8166e88328a7afdc20e417e63410c1e994
Maheshwar Anup <137691824+djmahe4@users.noreply.github.com>
2023-12-01 20:11:42 +0530

    Update def.py

--- def.py
+++ def.py
@@ -7,12 +7,17 @@ from youtubesearchpython import SearchVideos
 def download_audio(url, output_path):
     # Download the video
     video = YouTube(url)
+
+    # Check if the video is age-restricted
+    if video.age_restricted:
+        print("Skipping age-restricted video")
+        return
     stream = video.streams.filter(only_audio=True).first()
     output_file = stream.download(output_path=output_path)
 
     # Convert the file to mp3
     clip = AudioFileClip(output_file)
-    clip.write_audiofile(output_file.replace(".mp4", ".mp3"))
+    clip.write_audiofile(output_file.replace(".mp4", ".mp3"),bitrate="320k")
 
     # Remove the original file
     os.remove(output_file)
